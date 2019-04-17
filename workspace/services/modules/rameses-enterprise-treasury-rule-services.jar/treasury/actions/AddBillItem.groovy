ff459e35c856f93f83867adc3f06bf9fb5e559bf
LGU-Aklan-Altavas <lgu-aklan-altavas@gmail.com>
2019-04-17 13:18:47 +0800

    added services/modules

---
+++ workspace/services/modules/rameses-enterprise-treasury-rule-services.jar/treasury/actions/AddBillItem.groovy
@@ -0,0 +1,37 @@
+package treasury.actions;
+
+import com.rameses.rules.common.*;
+import com.rameses.util.*;
+import java.util.*;
+import treasury.facts.*;
+import com.rameses.osiris3.common.*;
+
+
+/***
+* Description: Simple Add of Item. Item is unique based on the account. 
+* Parameters:
+*    account 
+*    amount
+****/
+class AddBillItem extends AbstractAddBillItem {
+
+	public void execute(def params, def drools) {
+
+		def acct = params.account;
+		def amt = params.amount.decimalValue;
+
+		def remarks = null;
+		if( params.remarks ) {
+			remarks = params.remarks.eval();		
+		}
+
+		def billitem = new BillItem(amount: NumberUtil.round( amt));
+		billitem.remarks = remarks;
+		if( params.txntype?.key ) {
+			billitem.txntype = params.txntype.key;
+		}
+		setAccountFact( billitem, acct.key );
+		addToFacts( billitem );
+	}
+
+}
---
+++ workspace/services/modules/rameses-enterprise-treasury-rule-services.jar/treasury/facts/BillItem.groovy
@@ -0,0 +1,58 @@
+package treasury.facts;
+
+import java.util.*;
+import com.rameses.util.*;
+
+class BillItem extends AbstractBillItem {
+
+	String refid;
+	String ledgertype;
+
+	public BillItem(def o) {
+		copy(o);
+	}
+
+	//pay priority is only used during apply payment and will not be used anywhere else. This is defined by the extending class.
+	int paypriority = 0;
+	
+	LinkedHashSet<BillSubItem> items = new LinkedHashSet<BillSubItem>();
+
+	public def getTotals( def txntype ) {
+		return items.findAll{ it.txntype == txntype }.sum{it.amount};
+	}
+
+	public double getTotal() {
+		if(items.size()>0) {
+			return  NumberUtil.round( amount + items.sum{ it.amount } );		
+		}
+		else {
+			return NumberUtil.round( amount );
+		}
+	};
+
+	public int hashCode() {
+		if( refid ) {
+			return refid.hashCode();
+		} 
+		else {
+			return super.hashCode();
+		}
+	}
+
+	public def toMap() {
+		def m = super.toMap();
+		m.refid = refid;
+		m.ledgertype = ledgertype;
+		items.each {
+			if(it.amount == null) it.amount = 0;
+			m.put(it.txntype, NumberUtil.round(it.amount));
+		};
+		m.total = total;
+		return m;
+	}
+
+	//call this after apply payment
+	void recalc() {;}
+
+
+}
